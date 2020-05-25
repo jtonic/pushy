c88def7455de2a4c1f3182f285d33f5becfdfd76
jtonic <antonel.pazargic@gmail.com>
2020-05-25 08:02:26 +0300

    pushy client/mock server with CA, certificate and key.

---
+++ pushy/src/test/java/com/eatthepath/pushy/apns/jtonic/selfsigned/ApnsClientApp.java
@@ -0,0 +1,86 @@
+package com.eatthepath.pushy.apns.jtonic.selfsigned;
+
+import com.eatthepath.pushy.apns.ApnsClient;
+import com.eatthepath.pushy.apns.ApnsClientBuilder;
+import com.eatthepath.pushy.apns.PushNotificationResponse;
+import com.eatthepath.pushy.apns.util.ApnsPayloadBuilder;
+import com.eatthepath.pushy.apns.util.SimpleApnsPayloadBuilder;
+import com.eatthepath.pushy.apns.util.SimpleApnsPushNotification;
+import io.netty.channel.nio.NioEventLoopGroup;
+import java.time.Duration;
+import java.util.Random;
+import java.util.UUID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ApnsClientApp {
+
+  public static final Logger LOGGER = LoggerFactory.getLogger("apns-client");
+
+
+  private static final String HOST = "localhost";
+  private static final int PORT = 8443;
+  private static final NioEventLoopGroup CLIENT_EVENT_LOOP_GROUP = new NioEventLoopGroup(1);
+  private static final String CLIENT_CERTIFICATES_FILENAME = "/jtonic/ca/multi-topic-client.p12";
+  private static final String CA_CERTIFICATE_FILENAME = "/jtonic/ca/ca.pem";
+
+  private static final String CLIENT_PWD = "pushy-test";
+  private static final String TOPIC = "com.relayrides.pushy";
+
+  protected static final int TOKEN_LENGTH = 32; // bytes
+  private static final String DEVICE_TOKEN = generateRandomDeviceToken();
+  private static final String PAYLOAD = generateRandomPayload();
+
+  public static void main(String[] args) throws Exception {
+    ApnsClient client = null;
+    try {
+      client = new ApnsClientBuilder()
+          .setApnsServer(HOST, PORT)
+          .setTrustedServerCertificateChain(
+              ApnsClientApp.class.getResourceAsStream(CA_CERTIFICATE_FILENAME))
+          .setClientCredentials(
+              ApnsClientApp.class.getResourceAsStream(CLIENT_CERTIFICATES_FILENAME),
+              CLIENT_PWD)
+          .setEventLoopGroup(CLIENT_EVENT_LOOP_GROUP)
+          .setConnectionTimeout(Duration.ofSeconds(5))
+          .setMetricsListener(null)
+          .build();
+
+      final SimpleApnsPushNotification pushNotification = new SimpleApnsPushNotification(
+          DEVICE_TOKEN, TOPIC, PAYLOAD);
+
+      final PushNotificationResponse<SimpleApnsPushNotification> response =
+          client.sendNotification(pushNotification).get();
+
+      LOGGER.info("Response APNsId: {}", response.getApnsId());
+      assert (response.isAccepted());
+    } finally {
+      if (client != null) {
+        LOGGER.info("Closing the APNs client connection...");
+        client.close().get();
+        CLIENT_EVENT_LOOP_GROUP.shutdownGracefully().await();
+        LOGGER.info("Connection closed.");
+      }
+    }
+  }
+
+  protected static String generateRandomDeviceToken() {
+    final byte[] tokenBytes = new byte[TOKEN_LENGTH];
+    new Random().nextBytes(tokenBytes);
+
+    final StringBuilder builder = new StringBuilder(TOKEN_LENGTH * 2);
+
+    for (final byte b : tokenBytes) {
+      builder.append(String.format("%02x", b));
+    }
+
+    return builder.toString();
+  }
+
+  protected static String generateRandomPayload() {
+    final ApnsPayloadBuilder payloadBuilder = new SimpleApnsPayloadBuilder();
+    payloadBuilder.setAlertBody(UUID.randomUUID().toString());
+
+    return payloadBuilder.build();
+  }
+}
---
+++ pushy/src/test/java/com/eatthepath/pushy/apns/jtonic/selfsigned/MockApnsServerApp.java
@@ -0,0 +1,57 @@
+package com.eatthepath.pushy.apns.jtonic.selfsigned;
+
+import com.eatthepath.pushy.apns.ApnsPushNotification;
+import com.eatthepath.pushy.apns.server.AcceptAllPushNotificationHandlerFactory;
+import com.eatthepath.pushy.apns.server.MockApnsServer;
+import com.eatthepath.pushy.apns.server.MockApnsServerBuilder;
+import com.eatthepath.pushy.apns.server.ParsingMockApnsServerListenerAdapter;
+import com.eatthepath.pushy.apns.server.RejectionReason;
+import io.netty.channel.nio.NioEventLoopGroup;
+import java.time.Instant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MockApnsServerApp {
+
+  public static final Logger LOGGER = LoggerFactory.getLogger("mock-apns-server");
+
+  private static final NioEventLoopGroup SERVER_EVENT_LOOP_GROUP = new NioEventLoopGroup(2);
+
+  protected static final String SERVER_CERTIFICATES_FILENAME = "/jtonic/ca/server-certs.pem";
+  protected static final String SERVER_KEY_FILENAME = "/jtonic/ca/server-key.pem";
+  protected static final String CA_CERTIFICATE_FILENAME = "/jtonic/ca/ca.pem";
+  public static final int PORT = 8443;
+
+  public static void main(String[] args) throws Exception {
+    final MockApnsServer apnsServer = new MockApnsServerBuilder()
+        .setTrustedClientCertificateChain(
+            MockApnsServerApp.class.getResourceAsStream(CA_CERTIFICATE_FILENAME))
+        .setServerCredentials(
+            MockApnsServerApp.class.getResourceAsStream(SERVER_CERTIFICATES_FILENAME),
+            MockApnsServerApp.class
+                .getResourceAsStream(SERVER_KEY_FILENAME), null)
+        .setEventLoopGroup(SERVER_EVENT_LOOP_GROUP)
+        .setHandlerFactory(new AcceptAllPushNotificationHandlerFactory())
+        .setListener(new TestMockApnsServerListener())
+        .build();
+
+    apnsServer.start(PORT);
+
+    LOGGER.info("Mock apns server started on port {}", PORT);
+  }
+
+  private static class TestMockApnsServerListener extends ParsingMockApnsServerListenerAdapter {
+
+    @Override
+    public void handlePushNotificationAccepted(final ApnsPushNotification pushNotification) {
+      LOGGER.info("Accepted push notification payload: {}", pushNotification.getPayload());
+    }
+
+    @Override
+    public void handlePushNotificationRejected(final ApnsPushNotification pushNotification,
+                                               final RejectionReason rejectionReason,
+                                               final Instant deviceTokenExpirationTimestamp) {
+      LOGGER.info("Rejected push notification payload: {}", pushNotification.getPayload());
+    }
+  }
+}
